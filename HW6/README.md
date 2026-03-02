8851d572251d2ace412d42888ef61345db393f9a
Emily "Quinn" McNamee <157075962+emily-mcnamee@users.noreply.github.com>
2026-03-02 15:29:42 -0700

    HW6 update

--- HW6/README.md
+++ HW6/README.md
@@ -3,9 +3,9 @@
 This assignment involves using generative AI tools for debugging and coding assisstance. I have reviewed and critically evaluated the AI-generated content and incorporated my original ideas and judgment.
 
 02262026 Things still need to do:
-* fix finalMessage so that it displays player info properly on end screen
-* finalScreen is locked into left side of screen, needs to be center
-* figure out how to make it so player has to do input form every refresh
-* ~~inputAge form needs to only accept numbers~~
+* ~~fix finalMessage so that it displays player info properly on end screen~~ <-- I forgot to call all of the stored info
+* ~~finalScreen is locked into left side of screen, needs to be center~~ <-- It started working correctly when I solved above
+* ~~figure out how to make it so player has to do input form every refresh~~ <-- Don't need to do that, local storage can keep that data and the website now starts on the form. 
+* ~~inputAge form needs to only accept numbers~~ <-- Just needed to look up Bootstrap's guide and read a forum for the data-bind class
 
 ### Reflection Questions:
--- HW6/game.js
+++ HW6/game.js
@@ -6,20 +6,30 @@ const checkerboard = document.getElementById("checkerboard");
 var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12"];
 var blankImagePath = "./images/BoC.jpg";
 var actualImages = new Array();
+
+// variable for player info
 var player = {"firstname":"", "lastname":"", "age":"",};
+
+// array for card matches
 var matchedCards = [];
+
+// These two variables are needed to check if they're a match
 var firstCard = null;
 var secondCard = null;
+
+// Game State Variable 
 var lockBoard = false; 
+
+// Attempt Counter Variable
 var attempts = 0;
 
 function printBlanks(){
+
     createRandomArray();
     // for loop
     for(var i = 0; i < imageTags.length; i++)
         document.getElementById(imageTags[i]).src = blankImagePath;
 }
-
 
     function createRandomArray(){
 
@@ -33,14 +43,24 @@ function printBlanks(){
         ];
 
         actualImages = actualimagePath.concat(actualimagePath); // duplicate
-        actualImages.sort(() => Math.random() - 0.5); // shuffle
 
+        //actualImages.sort(() => Math.random() - 0.5); // shuffle
+
+        /* Using the Fisher–Yates shuffle */
+        for (let i = actualImages.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+
+        //Javascript array destructuring assignment - takes end of array [j] and swaps it with value [i] 
+        [actualImages[i], actualImages[j]] = [actualImages[j], actualImages[i]];
+    }
+
+        // Check to make sure the function works
         console.log(actualImages);
     }
 
 function flipImage(number)
 {
-    // stop if board is locked (return function)
+    // stop if board is locked
     if (lockBoard) return;
 
     // stop if the cards are already matched
@@ -57,16 +77,21 @@ function flipImage(number)
         return;
     }
 
+    // Counting attempt mechanic - flipping every second card counts as an attempt
     attempts++;
     document.getElementById("attemptCounter").textContent = attempts;
     secondCard = number;
+
+    // STOP - LOCK BOARD to check if cards match
     lockBoard = true;
 
     checkForMatch();
 }
 
 function checkForMatch() {
+    // if first card and second card tags match, push them to the matchedCards array and then reset the variables. Otherwise, flip them back and reset the variables to null
     if (actualImages[firstCard] === actualImages[secondCard]){
+
         matchedCards.push(firstCard);
         matchedCards.push(secondCard);
 
@@ -100,9 +125,11 @@ function addPlayerinfo(event){
     //console.log(lastName);
          var age = document.getElementById("inputAge").value;
       //console.log(age);
+
     player.firstname = firstName;
     player.lastname = lastName;
     player.age = age;
+
     localStorage.setItem("playerInfo", JSON.stringify(player));
 
     window.location = "game.html";
@@ -121,15 +148,20 @@ function showFinalScreen(){
     // bring in player info
 
     /*2262026 for some reason the finalMessage doesn't play, only lists the final Attempts. */
+    /*03022026 forgot to parse the info from the playerInfo function*/
     var storedPlayer = JSON.parse(localStorage.getItem("playerInfo"));
     if (storedPlayer){
         document.getElementById("finalMessage").textContent =
-        "Great job" + storedPlayer.firstname + "!";
+        "Great job" + " " + storedPlayer.firstname + " " + storedPlayer.lastname + "!";
+        document.getElementById("finalMessage2").textContent = storedPlayer.age;
     }
 }
+
+
 function playerInfo()
 {
     var playerInformation = localStorage.getItem("playerInfo");
     player = JSON.parse(playerInformation);
-    console.log(player);
+    var str = "Name: " + player.firstname + " " + player.lastname + "<br>" +
+    "age: " + player.age + "<br>"
 }
